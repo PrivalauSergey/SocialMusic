39c5216dd2bf3e56dba8081d487da16c801a0281
RESIDEO\273255 <siarhei.privalau@resideo.com>
2025-01-26 16:45:05 +0300

    SM-5 init angular app

---
+++ src/SM.Home.UI/src/components/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signup',
+  standalone: true,
+  
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+}
